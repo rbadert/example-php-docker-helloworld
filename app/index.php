9a0c5e712daff5e0c42046cb6a6cf329d2e61877
rbadert <retobadertscher@gmail.com>
2017-03-09 16:59:47 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -54,7 +54,7 @@
 
       <div class="starter-template">
         <h1>Hallo <?php echo 'APPUiO'?></h1>
-        <p class="lead">APPUiO Example Dockerfile PHP</p>
+        <p class="lead">APPUiO Example Dockerfile PHP changed by RBAD!</p>
       </div>
 
     </div><!-- /.container -->
